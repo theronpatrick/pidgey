fd9bdb5367ff9b1187de1038f8d68c4150c24d6f
Theron Patrick <theronbpatrick@gmail.com>
2016-08-13 10:54:59 -0400

    basic setup for getting issues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './styles/App.scss';
 import IssueList from './components/IssueList';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log("loaded")
+
+
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React <IssueList></IssueList></h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit! <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Issue Viewer</h1>
+        <IssueList />
       </div>
     );
   }
--- src/components/IssueList.jsx
+++ src/components/IssueList.jsx
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
 import '../styles/IssueList.scss';
+import $ from 'jquery';
 
 class IssueList extends Component {
 
-	componentDidMount() {
-     console.log("oh hai")
+  constructor(props) {
+    super(props);
+
+    this._resourceUrl = "https://api.github.com/repos/rails/rails/issues";
+
+    this._resourceParams = {
+      per_page: 25,
+      page: 1
+    }
+
+    this.state = {
+      issues: []
+    }
+
   }
+
+	componentDidMount() {
+     console.log("oh hai " , this._resourceUrl);
+
+     $.ajax({
+        url: this._resourceUrl,
+        type: "GET",
+        data: this._resourceParams
+     })
+     .then((r)=> {
+      this.setState({
+        issues: r
+      })
+     })
+
+
+  }
+
+
 
   _doThing() {
     return "DOIT"
   }
 
   render() {
+
+    // TODO: Make an 'issue' component?
+    let rows = [];
+    for (let i = 0; i < this.state.issues.length; i++) {
+        rows.push(<li key={i}>{this.state.issues[i].title}</li>);
+    }
+
     return (
-      <div className="test">testeroo {this._doThing()}</div>
+      <div className="test">
+        <ul>
+          {rows}
+        </ul>
+      </div>
     );
   }
 }
